eebed71ec8f38c275468e8402512bdaa98353754
Jarrel Armster <jarrel@enversedesign.com>
2016-03-08 22:59:03 +0000

    Variables

---
+++ name.rb
@@ -0,0 +1,9 @@
+puts "Please type your First name"
+first_name = gets
+puts "Please type your last name"
+last_name = gets
+puts "Hi" + first_name + " " + last_name
+
+# 10.times do |n|
+#   puts name
+# end
