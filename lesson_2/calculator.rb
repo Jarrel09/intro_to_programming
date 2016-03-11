c38fc8604060ec21c361bf3b9e6e9ff614f69282
Jarrel Armster <jarrel@enversedesign.com>
2016-03-11 18:13:41 +0000

    Rubocop Calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -23,22 +23,19 @@ prompt('Welcome to Calculator! Enter your name:')
 name = ''
 loop do
   name = Kernel.gets.chomp
-  
   if name.empty?
     prompt("Make sure you entered a valid name.")
   else
     break
   end
 end
-
 prompt("Hi #{name}!")
 
 loop do
   number1 = ''
-  loop do  
+  loop do
     prompt("What's the first number")
     number1 = Kernel.gets.chomp
-    
     if valid_number?(number1)
       break
     else
@@ -50,27 +47,23 @@ loop do
   loop do
     prompt("What's the second number?")
     number2 = Kernel.gets.chomp
-    
     if valid_number?(number2)
       break
     else
       prompt("Hmmm..that doesn't look like a valid number")
     end
-  end 
+  end
   
   operator_prompt = <<-MSG
     What operation would you like to perform?
-    1) add 
-    2) subtract 
-    3) multiply 
+    1) add
+    2) subtract
+    3) multiply
     4) divide
   MSG
   prompt(operator_prompt)
-  
   operator = Kernel.gets.chomp
   loop do
-    
-    
     if %w(1 2 3 4).include?(operator)
       break
     else
@@ -81,18 +74,16 @@ loop do
   prompt("#{operation_to_message(operator)} the two numbers...")
   
   result = case operator
-            when '1'
-              number1.to_i + number2.to_i 
-            when '2'
-              number1.to_i - number2.to_i
-            when '3'
-              number1.to_i * number2.to_i
-            when '4'
-              number1.to_f / number2.to_f
-              
-            end
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
   prompt("The result is #{result}")
-  
   prompt("Do you want to perform another calculation? (Y to calculate again)")
   answer = Kernel.gets.chomp
   break unless answer.downcase.start_with?('y')
