ecf2d00bc8744c40f077463fe89ea812da542f61
Jarrel Armster <jarrel@enversedesign.com>
2016-03-15 21:27:52 +0000

    Easy Exercises 3

---
+++ lesson_3/easy_exercises_3.rb
@@ -0,0 +1,20 @@
+# Question 1
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# Question 2
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# flintstones << "Dino"
+
+# Question 3
+# flintstones.push("Dino").push("Hoppy")
+
+# Question 4
+# advice.slice!(0, advice.index('house'))
+
+# Question 5
+# statement = "The Flintstones Rock!"
+# statement.count('t')
+
+# Question 6
+# title = "Flintstone Family Members"
+# title.center(40)
