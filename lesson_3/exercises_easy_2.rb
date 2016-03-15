d6687e58fd34beef3754a40412d781d70c4b2030
Jarrel Armster <jarrel@enversedesign.com>
2016-03-15 21:09:12 +0000

    Exercises Easy 2

---
+++ lesson_3/exercises_easy_2.rb
@@ -0,0 +1,49 @@
+# Question 1
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+# ages(:spot)
+# Bonus: ages.include?("spot") 
+#        ages.memeber?("spot")
+
+# Question 2
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10,
+# "Marilyn" => 22, "Spot" => 237 }
+# total = 0
+# ages.each_value { |value| total += value}
+# total
+
+# Question 3
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, 
+# "Marilyn" => 22, "Spot" => 237 }
+# ages.keep_if {|key, value| value < 100}
+
+# Question 4
+# munsters_description = "The Munsters are creepy in a good way."
+# - munsters_description.capitalize!
+# - munsters_description.swapcase!
+# - munsters_description.downcase!
+# - munsters_description.upcase!
+
+# Question 5
+# ages.merge!(additional_ages)
+
+#Question 6
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10,
+# "Marilyn" => 22, "Spot" => 237 }
+# ages.values.min
+
+# Question 7
+# advice = "Few things in life are as important as house training your pet
+# dinosaur."
+# advice.include?('Dino')
+
+# Question 8
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# ?
+
+# Question 9
+# flintstones.map! { |name| name[0,3] }
+
+# Question 10
+# flintstones.map! { |name| name[0,3] }
+
+
